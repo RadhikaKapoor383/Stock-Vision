889c81a87f9c253d26d553c2221b83eb3e080760
Radhika kapoor <radhikakapoor383@gmail.com>
2026-06-12 02:53:30 +0500

    feat: add initial App component and update package dependencies

---
+++ stock-dashboard/src/App.jsx
@@ -0,0 +1,10 @@
+import MainLayout from './components/layout/MainLayout';
+function App() {
+  return (
+    <MainLayout>
+      <h1>Stock Dashboard</h1>
+    </MainLayout>
+  )
+}
+
+export default App
